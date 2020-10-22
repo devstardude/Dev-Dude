46c4a1f79a245bf444952fcbfe55cadaa55d7f9a
Dude <arunshekhar001@gmail.com>
2020-10-23 05:07:00 +0530

    desktop ready

--- src/components/Landing/Components/Header.js
+++ src/components/Landing/Components/Header.js
@@ -4,11 +4,13 @@ import React from "react";
 import "./Header.css";
 
 const Header = (props) => {
+  let date = new Date().getHours();
+  let greeting = (date>=6 && date<=18)? "Buon Giorno ☀" : "Buon Serata 🌟";
   return (
     <React.Fragment>
       <div className="text-center text-md-left mt-md-3">
         <Fade in={true} timeout={700}>
-          <h3 className="Header-text">Buon Giorno ☀</h3>
+          <h3 className="Header-text">{greeting}</h3>
         </Fade>
       </div>
     </React.Fragment>
